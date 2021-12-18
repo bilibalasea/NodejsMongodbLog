8fe89cf54da15914a59490db6661d50afe65ad8a
gmq <ganmengqing0@163.com>
2021-12-18 00:20:06 -0800

    add some home pages and functions

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -3,18 +3,69 @@ const app = express()
 const mongoose = require('mongoose');
 const ejs = require('ejs')
 mongoose.connect('mongodb://172.21.2.236:27017/190110910706');
+
 const schema={
-    name: String,
-    age: Number,
-    health: String,
-    hooby:String
-}
-const mydata = mongoose.model('cat1s', schema);
-// const kitty = new mydata({ name: 'testZildjian2' });
-// kitty.save()
+        username: {type: String, require: true},
+        password: {type: String, require: true},
+        age:Number,
+        sex: String,
+        tel: Number,
+        dor: String,
+        role: {type: String, require: true},
+    }
+const userdata = mongoose.model('user', schema);
+// const user1 = new userdata({ username: 'admin' , password:'001' , sex:'female' , tel:'123456',role:'manager'});
+// user1.save();
+
+mongoose.connection.on('connected', () => {
+    console.log('mongodb connect success')
+})
 
 app.use('/',express.static('public'))
 app.use('/',express.static('views'))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+var loguser="";
+var logpassword="";
+var logrole="";
+app.post('/login', function (req, res, next){
+    // console.log(req.body)
+    loguser=req.body.username;
+    logpassword=req.body.password;
+    logrole=req.body.role
+    
+    // 查询数据库
+    userdata.find({"username":loguser},(err, doc) => {
+        if (!err) {
+            console.log(doc)
+        } else {
+            console.log(err)
+        }
+        finddata=doc[0]['_doc']
+        // console.log(loguser==finddata['username'])
+        //用户信息验证
+        if(loguser==finddata['username']&&logpassword==finddata['password']&&logrole==finddata['role']){
+            console.log('okok')
+            res.setHeader('Content-Type', 'text/html');
+            ejs.renderFile('views/home.html', function(err, str){
+                // str => 输出渲染后的 HTML 字符串
+                if(err){console.log(err)}
+                else{
+                    res.statusCode = 200;
+                    
+                     res.end(str)
+                }
+              });
+        }
+        else {
+            alert("用户名、密码或身份选择错误，请重新登录");
+        }
+    })
+    next();
+})
+
 app.get("/input",(req,res,next)=>{
     // res.send(req.query)
     // console.log(req.query)
@@ -29,13 +80,28 @@ app.get("/input",(req,res,next)=>{
     next();
 })
 
-app.get("/reg",(req,res,next)=>{
+app.post("/reg",(req,res,next)=>{
+    console.log(req.body)
+    var reguser=req.body.username;
+    var regpassword=req.body.password;
+    var regage=parseFloat(req.body.age);
+    var regsex=req.body.sex;
+    var regtel=parseFloat(req.body.tel);
+    var regdor=req.body.dor;
+    var regrole=req.body.role
+    const userreg = new userdata({ username: reguser , password:regpassword , sex:regage , 
+                                    tel:regtel,role:regrole});
+    userreg.save();
     // res.send(req.query)
-    console.log(req.query)
-    res.sendFile(--__dirname+"/views/reg.html",(err)=>{
-        if(err)
-            console.log(err)
-    })
+    if(regrole=="mannger")
+        res.sendFile(path.join(__dirname, 'views/manager/home.html', 'test.html'));
+    else
+        res.sendFile(path.join(__dirname, 'views/student/home.html', 'test.html'));
+    res.end();
+    // res.sendFile(__dirname+"/views/reg.html",(err)=>{
+    //     if(err)
+    //         console.log(err)
+    // })
     next();
 })
 app.listen(10706)
