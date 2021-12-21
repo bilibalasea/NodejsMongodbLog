69cc22ced67ee720481753bbf1f0c5633e0ccdab
gmq <ganmengqing0@163.com>
2021-12-20 21:53:36 -0800

    add some actions about managers and students

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -11,6 +11,7 @@ const schema1={
         sex: String,
         tel: Number,
         dor: String,
+        room: String,
         role: {type: String, require: true},
     }
 const userdata = mongoose.model('user', schema1);
@@ -20,8 +21,11 @@ const userdata = mongoose.model('user', schema1);
 const schema2={
     notice: String,
     editor: String,
+    valid: String
 }
 const noticedata = mongoose.model('notice', schema2);
+// const notice1 = new noticedata({ notice: '明后天天气晴朗，天台将会开放，同学们可于明后两日在天台按秩序晒被子' , editor:'qwe' , valid:'有效'});
+// notice1.save();
 
 mongoose.connection.on('connected', () => {
     console.log('mongodb connect success')
@@ -40,17 +44,12 @@ app.post("/login",function(req,res,next){
     loguser=String(req.body.username);
     logpassword=String(req.body.password);
     logrole=String(req.body.role);
-    console.log("post ok")
-    console.log(logrole)
     next();
 })
 
 app.use('/login', function (req, res, next){
     userdata.findOne({ username: loguser },  function (err, userdata) {
         if (err) return handleError(err);
-        console.log(logpassword == userdata.password)
-        console.log(logrole==userdata.role)
-
         if(logpassword == userdata.password&&logrole == userdata.role){
             if(logrole=='manager'){
                 ejs.renderFile('public/views/manager/home.html',function(err, str){
@@ -91,20 +90,6 @@ app.use('/login', function (req, res, next){
     // next();
 });
 
-app.get("/input",(req,res,next)=>{
-    // res.send(req.query)
-    // console.log(req.query)
-    // const kitty = new mydata({ name: req.query.first,health: req.query.second });
-    // kitty.save()
-    // ejs.renderFile(filename, data, options, function(err, str){
-    //     // str => 输出渲染后的 HTML 字符串
-    // });
-    ejs.renderFile("result.html",{returnVal:"seccess"},(err,str)=>{
-        res.send(str)
-    })
-    next();
-})
-
 app.post("/reg",(req,res,next)=>{
     console.log(req.body)
     reguser=req.body.username;
@@ -113,15 +98,16 @@ app.post("/reg",(req,res,next)=>{
     regsex=req.body.sex;
     regtel=parseFloat(req.body.tel);
     regdor=req.body.dor;
+    regroom=req.body.room;
     regrole=req.body.role
     loguser=reguser
-    const userreg = new userdata({ username: reguser , password:regpassword , sex:regage , 
-                                    tel:regtel,role:regrole});
+    const userreg = new userdata({ username: reguser , password:regpassword , age:regage , sex:regsex , 
+                                    tel:regtel, dor:regdor, room:regroom, role:regrole});
     userreg.save();
 
     // res.send(req.query)
     if(regrole=="mannger"){
-        ejs.renderFile('public/manager/home.html', {user:reguser},function(err, str){
+        ejs.renderFile('public/views/manager/home.html', function(err, str){
             // str => 输出渲染后的 HTML 字符串
             if(err){console.log(err)}
             else{
@@ -132,7 +118,7 @@ app.post("/reg",(req,res,next)=>{
         // res.sendFile(path.join(__dirname, 'public/views/manager/home.html', 'test.html'));
     }
     else{
-        ejs.renderFile('public/student/home.html', {user:reguser},function(err, str){
+        ejs.renderFile('public/views/student/home.html', function(err, str){
             // str => 输出渲染后的 HTML 字符串
             if(err){console.log(err)}
             else{
@@ -143,12 +129,91 @@ app.post("/reg",(req,res,next)=>{
     }
     next();
 })
+
+//管理员所有寝室信息
+app.use("/allDorInfo",(req,res,next)=>{
+    userdata.find({}, function (err, userdata1) {
+        ejs.renderFile('public/views/manager/allDorInfo.html', {searchdorinfo:userdata1},function(err, str){
+            // str => 输出渲染后的 HTML 字符串
+            if(err){console.log(err)}
+            else{
+                res.setHeader('Content-Type', 'text/html');
+                res.end(str)
+            }
+        });
+    })
+})
+
+//按寝室楼幢号搜索
+var searchDor=""
+app.post("/dorsearch",function(req,res,next){
+    searchDor=String(req.body.dorser);
+    next();
+})
+app.use("/dorsearch",(req,res,next)=>{
+    if (searchDor==""){
+        userdata.find({}, function (err, userdata1) {
+            ejs.renderFile('public/views/manager/allDorInfo.html', {searchdorinfo:userdata1},function(err, str){
+                // str => 输出渲染后的 HTML 字符串
+                if(err){console.log(err)}
+                else{
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end(str)
+                }
+            });
+        })
+    }else{
+        userdata.find({dor:searchDor}, function (err, userdata1) {
+            ejs.renderFile('public/views/manager/allDorInfo.html', {searchdorinfo:userdata1},function(err, str){
+                // str => 输出渲染后的 HTML 字符串
+                if(err){console.log(err)}
+                else{
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end(str)
+                }
+            });
+        })
+    }
+})
+
+//按房间号搜索
+var searchRoom=""
+app.post("/roomsearch",function(req,res,next){
+    searchRoom=String(req.body.room);
+    next();
+})
+app.use("/roomsearch",(req,res,next)=>{
+    if (searchRoom==""){
+        userdata.find({}, function (err, userdata1) {
+            ejs.renderFile('public/views/manager/allDorInfo.html', {searchdorinfo:userdata1},function(err, str){
+                // str => 输出渲染后的 HTML 字符串
+                if(err){console.log(err)}
+                else{
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end(str)
+                }
+            });
+        })
+    }else{
+        userdata.find({room:searchRoom}, function (err, userdata1) {
+            ejs.renderFile('public/views/manager/allDorInfo.html', {searchdorinfo:userdata1},function(err, str){
+                // str => 输出渲染后的 HTML 字符串
+                if(err){console.log(err)}
+                else{
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end(str)
+                }
+            });
+        })
+    }
+})
+
 //管理员发布公告
 app.post("/notice",(req,res,next)=>{
     console.log(req.body)
     adnotice=req.body.notice;
     adeditor=req.body.editor;
-    const addnotice = new noticedata({ notice:adnotice , editor:adeditor});
+    const addnotice = new noticedata({ notice:adnotice , editor:adeditor, valid:'有效'});
     addnotice.save();
     ejs.renderFile('public/views/manager/Addnotice.html', function(err, str){
         // str => 输出渲染后的 HTML 字符串
@@ -165,7 +230,7 @@ app.use("/perinfo",(req,res,next)=>{
     userdata.findOne({ username: loguser },  function (err, userdata) {
         if (err) return handleError(err);
         ejs.renderFile('public/views/manager/perInfo.html', {username: userdata.username, age:userdata.age,
-        sex: userdata.sex,tel: userdata.tel,dor: userdata.dor,role: userdata.role},function(err, str){
+        sex: userdata.sex,tel: userdata.tel,dor: userdata.dor,room: userdata.room,role: userdata.role},function(err, str){
             // str => 输出渲染后的 HTML 字符串
             if(err){console.log(err)}
             else{
@@ -176,11 +241,94 @@ app.use("/perinfo",(req,res,next)=>{
    });
 })
 
+//管理员查看公告
+app.use("/managernotice",(req,res,next)=>{
+    noticedata.find({}, function (err, noticedata1) {
+        if (err) return handleError(err);
+        ejs.renderFile('public/views/manager/notice.html', {allnotice:noticedata1},function(err, str){
+            // str => 输出渲染后的 HTML 字符串
+            if(err){console.log(err)}
+            else{
+                res.setHeader('Content-Type', 'text/html');
+                res.end(str)
+            }
+        })
+   });
+})
+
+//管理员修改公告状态
+app.post("/valid",function(req,res,next){
+    // console.log(req.query)
+    alterid=String(req.query.id);
+    next();
+})
+app.use("/valid",function(req,res,next){
+    noticedata.findOne({ _id: alterid },  function (err, noticedata1) {
+        if (err) return console.log(err);
+        if(noticedata1.valid=='有效'){
+            noticedata.updateOne({ _id: alterid }, {valid: "无效"} ,function (err, noticedata2) {
+                if (err) return console.log(err);
+                // console.log('修改成功') 
+                noticedata.find({}, function (err, noticedata1) {
+                    if (err) return console.log(err);
+                    ejs.renderFile('public/views/manager/notice.html', {allnotice:noticedata1},function(err, str){
+                        // str => 输出渲染后的 HTML 字符串
+                        if(err){console.log(err)}
+                        else{
+                            res.setHeader('Content-Type', 'text/html');
+                            res.end(str)
+                        }
+                    })
+               });
+            }); 
+        }else{
+            noticedata.updateOne({ _id: alterid }, {valid: "有效"} ,function (err, noticedata2) {
+                if (err) return console.log(err);
+                // console.log('修改成功') 
+            }); 
+            noticedata.find({}, function (err, noticedata1) {
+                if (err) return console.log(err);
+                ejs.renderFile('public/views/manager/notice.html', {allnotice:noticedata1},function(err, str){
+                    // str => 输出渲染后的 HTML 字符串
+                    if(err){console.log(err)}
+                    else{
+                        res.setHeader('Content-Type', 'text/html');
+                        res.end(str)
+                    }
+                })
+           });
+        }
+    });
+})
+
+//管理员删除公告操作
+app.post("/delete",function(req,res,next){
+    // console.log(req.query)
+    alterid=String(req.query.id);
+    next();
+})
+app.use("/delete",function(req,res,next){
+    noticedata.deleteOne({ _id: alterid },  function (err, noticedata1) {
+        if (err) return console.log(err);
+        noticedata.find({}, function (err, noticedata1) {
+            if (err) return console.log(err);
+            ejs.renderFile('public/views/manager/notice.html', {allnotice:noticedata1},function(err, str){
+                // str => 输出渲染后的 HTML 字符串
+                if(err){console.log(err)}
+                else{
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end(str)
+                }
+            })
+        });
+    }); 
+})
+
 app.use("/stuinfo",(req,res,next)=>{
     userdata.findOne({ username: loguser },  function (err, userdata) {
         if (err) return handleError(err);
-        ejs.renderFile('public/views/manager/perInfo.html', {username: userdata.username,age:userdata.age,
-        sex: userdata.sex,tel: userdata.tel,dor: userdata.dor,role: userdata.role},function(err, str){
+        ejs.renderFile('public/views/student/perInfo.html', {username: userdata.username,age:userdata.age,
+        sex: userdata.sex,tel: userdata.tel,dor: userdata.dor,room: userdata.room,role: userdata.role},function(err, str){
             // str => 输出渲染后的 HTML 字符串
             if(err){console.log(err)}
             else{
@@ -192,22 +340,26 @@ app.use("/stuinfo",(req,res,next)=>{
 })
 
 app.use("/mydorInfo",(req,res,next)=>{
-    userdata.findOne({ username: loguser },  function (err, userdata) {
+    userdata.findOne({ username: loguser },  function (err, userdata0){
         if (err) return handleError(err);
-        ejs.renderFile('public/views/manager/perInfo.html', {username: userdata.username,age:userdata.age,
-        sex: userdata.sex,tel: userdata.tel,dor: userdata.dor,role: userdata.role},function(err, str){
-            // str => 输出渲染后的 HTML 字符串
-            if(err){console.log(err)}
-            else{
-                res.setHeader('Content-Type', 'text/html');
-                res.send(str)
-            }
+        logdor=userdata0.dor
+        logroom=userdata0.room
+        userdata.find({dor: logdor, room: logroom}, function (err, userdata1) {
+            if (err) return handleError(err);
+            ejs.renderFile('public/views/student/dorInfo.html', {dorinfo:userdata1},function(err, str){
+                    // str => 输出渲染后的 HTML 字符串
+                    if(err){console.log(err)}
+                    else{
+                        res.setHeader('Content-Type', 'text/html');
+                        res.send(str)
+                    }
+                })
         })
-   });
+    })
 })
 
 app.use("/readnotice",(req,res,next)=>{
-    noticedata.find({}, function (err, noticedata1) {
+    noticedata.find({valid:"有效"}, function (err, noticedata1) {
         if (err) return handleError(err);
         ejs.renderFile('public/views/student/notice.html', {allnotice:noticedata1},function(err, str){
             // str => 输出渲染后的 HTML 字符串
@@ -219,4 +371,18 @@ app.use("/readnotice",(req,res,next)=>{
         })
    });
 })
+
+// app.use("/logout",(req,res,next)=>{
+//     logname=""
+//     logpassword=""
+//     logrole=""
+//         ejs.renderFile('public/index.html' ,function(err, str){
+//             // str => 输出渲染后的 HTML 字符串
+//             if(err){console.log(err)}
+//             else{
+//                 res.setHeader('Content-Type', 'text/html');
+//                 res.end(str)
+//             }
+//         })
+// })
 app.listen(10706)
